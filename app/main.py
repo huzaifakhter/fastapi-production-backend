456e2c27cb5b5acd48148f98df68ac48022c7fb6
huzaifakhter <mail.huxaif@gmail.com>
2026-02-02 15:11:44 +0530

    feat: some fixes in the main.py file.

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from app.modules.auth.router import router
 from app.core.db import engine
 from app.modules.auth.models import Base
-from app.modules.test_service.routes import router as test_router
+from app.modules.blog.routes import router as blog
 
 app = FastAPI()
 app.include_router(router, prefix="/auth", tags=["auth"])
-app.include_router(test_router, prefix="/test", tags=["test"])
+app.include_router(blog, prefix="/blog", tags=["blog"])
 
 @app.on_event("startup")
 async def startup():
